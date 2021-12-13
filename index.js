688d03fd3393c6455d019ae2dc1c5a32dd5c8ef6
taylorvercillo <taylornvcasey@gmail.com>
2021-12-12 22:10:48 -0700

    commit tree 14de5dd9453ec079b100ac0f1c2bac55a2fee212

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+
+function submitData (name, email) {
+    const configObj = {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json', 
+            Accept: 'application/json'
+        },
+        body:JSON.stringify( {
+            name: "Steve", 
+            email: "steve@steve.com"
+        }) 
+    }
+    fetch('http://localhost:3000/users', configObj)
+    .then(res => res.json())
+    .then(body => toDom(body.id))
+    .catch(error => {
+        let span = document.createElement('span')
+        span.innerText = "Unauthorized Access"
+        document.body.append(span)
+    })
+}
+
+function toDom(theID) {
+    document.getElementsByTagName('body')[0].innerHTML = theID; 
+}
+
+
+submitData(name, email); 
